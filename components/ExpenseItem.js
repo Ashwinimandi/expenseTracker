57db3c183c32aac897e1051f7cdf97f5bb3fcd27
Ashwinimandi <ashwinimandi39@gmail.com>
2021-11-01 13:10:53 +0530

    added state

--- components/ExpenseItem.js
+++ components/ExpenseItem.js
@@ -1,25 +1,34 @@
+import React, { useState } from 'react';
+import ExpenseDate from './ExpenseDate';
+import Card from './Card';
 import './ExpenseItem.css';
 
 function ExpenseItem(props) {
-  const month=props.date.toLocaleString('en-US',{month:'long'});
-  const day=props.date.toLocaleString('en-US',{day:'2-digit'});
-  const year=props.date.getFullYear();
-  
+  const [title,setTitle]=useState(props.title);
+
+  const clickHandler=()=>
+  {
+    setTitle('Updated!');
+    console.log(title);
+  };
+  /*const deleteExpense=()=>
+  {
+    const parentNode = document.getElementById('listOfUsers');
+    const childNodeToBeDeleted = document.getElementById(emailId);
+
+    parentNode.removeChild(childNodeToBeDeleted)
+  }*/
+
   return (
-    <div className="expense-item">
-      <div>
-      <div>{month}</div>
-      <div>{year}</div>
-      <div>{day}</div>
+    <Card className='expense-item'>
+      <ExpenseDate date={props.date} />
+      <div className='expense-item__description'>
+        <h2>{title}</h2>
+        <div className='expense-item__price'>${props.amount}</div>
       </div>
-      <div className="expense-item__description">
-        
-        <h2>{props.title}</h2>
-        <h2>Bengaluru</h2>
-        <div className="expense-item__price">${props.amount}</div>
-        
-      </div>
-    </div>
+      <button onClick={clickHandler}>Change Title</button>
+      <button >Delete</button>
+    </Card>
   );
 }
 
